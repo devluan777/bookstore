2edd700b39e52477ca9bf09fb262597a6fa5c2c1
devluan777 <zcanaldedois@gmail.com>
2025-05-13 19:28:06 -0300

    Corrige erro no ViewSet: ajuste na validação de parâmetros

--- bookstore/order/models/order.py
+++ bookstore/order/models/order.py
@@ -9,3 +9,6 @@ from product.models.product import Product
 class Order (models.Model):
     product = models.ManyToManyField(Product , blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'order'
