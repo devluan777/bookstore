fa8eca80171c3c8c05a440bc72d388e294949d5a
devluan777 <zcanaldedois@gmail.com>
2025-07-29 00:04:10 -0300

    Adicionando branch para correção dos erros

--- bookstoreapp/settings.py
+++ bookstoreapp/settings.py
@@ -31,16 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "rest_framework",
-    "order",
-    "product",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'rest_framework',
+    'bookstoreapp',
+
+    'order',
+    'product',
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -124,3 +129,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
--- product/models/product.py
+++ product/models/product.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 from django.db import models
 
-from .category import Category
-
-
-
+from product.models import Category
 
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=500, blank=True, null=True)
-    price = models.PositiveIntegerField(null=True)
-    active = models.BooleanField(default=True)
-    category = models.ManyToManyField(Category, blank=True)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'product'
 
 
+    def __str__(self):
+        return self.title
+      
